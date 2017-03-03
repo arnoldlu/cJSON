0373e23d4f035301ffa4388a69e0fad86963fb70
Arnold Lu <arnoldlu@qq.com>
2017-03-03 09:29:08 +0800

    make script for cJSON
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

---
+++ make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#mkdir build
+#cd build
+#cmake .. -DENABLE_CJSON_UTILS=Off -DENABLE_CJSON_TEST=On -DCMAKE_INSTALL_PREFIX=/usr （生成bin+lib）
+#cmake .. -DENABLE_CJSON_UTILS=Off -DENABLE_CJSON_TEST=On -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_SHARED_LIBS=Off （生成bin）
+#make
+#sudo make install （安装libcjson.so）
